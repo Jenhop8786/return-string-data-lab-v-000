dd8a13d027de3b85b7121262c36df904f09c2bf2
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-09-22 03:12:26 +0000

    automatically backed up by learn

--- db/migrate/20180921234551_add_inventory_and_description_to_products.rb
+++
@@ -1,6 +0,0 @@
-class AddInventoryAndDescriptionToProducts < ActiveRecord::Migration
-  def change
-    add_column :products, :inventory, :string
-    add_column :products, :description, :string 
-  end
-end
